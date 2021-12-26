63286a57b15d63f80160a08d84e1705ca56ec489
Smug B <43657791+SmugBlanco@users.noreply.github.com>
2021-12-26 17:08:54 -0500

    I SAW THE IMPOSTAH VENT
    I SWORE

---
+++ NPCs/Data/DownedBossData.cs
@@ -0,0 +1,72 @@
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace TheDestinyMod.NPCs.Data
+{
+    public struct DownedBossData
+    {
+        public int Type;
+
+        public string ModName;
+
+        public string NPCName;
+
+        public DownedBossData(int type)
+        {
+            Type = type;
+            ModNPC modNPC = ModContent.GetModNPC(type);
+            if (modNPC == null)
+            {
+                ModName = string.Empty;
+                NPCName = string.Empty;
+            }
+            else
+			{
+                ModName = modNPC.mod.Name;
+                NPCName = modNPC.Name;
+            }
+        }
+
+        public DownedBossData(int type, string modName, string npcName)
+        {
+            Type = type;
+            ModName = modName;
+            NPCName = npcName;
+        }
+
+        public TagCompound Save()
+        {
+            return new TagCompound()
+            {
+                { "Type", Type },
+                { "ModName", ModName },
+                { "NPCName", NPCName },
+            };
+		}
+
+        public static DownedBossData Load(TagCompound tagCompound)
+        {
+            int type = tagCompound.Get<int>("Type");
+            string modName = tagCompound.Get<string>("ModName");
+            string npcName = tagCompound.Get<string>("NPCName");
+            if (modName == string.Empty)
+            {
+                return new DownedBossData(type, modName, npcName);
+            }
+
+            Mod mod = ModLoader.GetMod(modName);
+            if (mod == null)
+            {
+                return new DownedBossData(-1, modName, npcName);
+            }
+
+            ModNPC modNPC = mod.GetNPC(npcName);
+            if (modNPC == null)
+			{
+                return new DownedBossData(-1, modName, npcName);
+            }            
+
+            return new DownedBossData(modNPC.npc.type, modName, npcName);
+        }
+    }
+}
