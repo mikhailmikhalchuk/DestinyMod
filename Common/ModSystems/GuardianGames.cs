5487e4f1921cee7852f700dbfa3e6d780170fbe1
Cuno <59270079+MikhailMCraft@users.noreply.github.com>
2022-02-16 19:21:14 -0500

    Server stuff

--- Common/ModSystems/GuardianGames.cs
+++ Common/ModSystems/GuardianGames.cs
@@ -11,6 +11,7 @@ using DestinyMod.Content.Items.Misc;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace DestinyMod.Common.ModSystems
 {
@@ -79,75 +80,78 @@ namespace DestinyMod.Common.ModSystems
 
 					item.SetDefaults();
 				}
-				
-				HttpClient client = new HttpClient();
-				client.Timeout = TimeSpan.FromSeconds(1.5);
-				client.BaseAddress = new Uri("https://DestinyModServer.mikhailmcraft.repl.co");
-				HttpRequestMessage request = new HttpRequestMessage();
-				request.Method = HttpMethod.Post;
-				request.Headers.Add("VERIFY-MOD", "a7rg53F435h4Ff2fhjWa33gH6j54ag2G");
-				request.Headers.Add("CLASS", classPlayer.ClassType.ToString().ToUpper());
-				request.Headers.Add("DUPLICATE-CHECK", Steamworks.SteamUser.GetSteamID().ToString());
-				HttpResponseMessage response = client.Send(request);
-				/*
-				request.Method = "POST";
-				request.ContentType = "application/x-www-form-urlencoded";
-				request.ContentLength = data.Length;*/
 
-				string postData = Uri.EscapeDataString(laurelCount.ToString());
-				byte[] data = System.Text.Encoding.ASCII.GetBytes(postData);
-
-				using (var stream = request.Content.ReadAsStream())
+				Task.Run(async () =>
 				{
-					stream.Write(data, 0, data.Length);
-				}
-				string responseString = new StreamReader(response.Content.ReadAsStream()).ReadToEnd();
-
-				if (responseString == "TOOMUCH")
-				{
-					Main.NewText("You've already deposited 30 Laurels today! Try again tomorrow.", Color.Red);
-					player.QuickSpawnItem(laurelType, laurelCount);
-				}
-				else
-				{
-					string depositedLaurelsString = responseString.Remove(responseString.IndexOf("RECEIVED"));
-					if (string.IsNullOrEmpty(depositedLaurelsString))
+					HttpClient client = new HttpClient
 					{
-						int depositedLaurels = int.Parse(depositedLaurelsString);
-						player.QuickSpawnItem(laurelType, laurelCount - depositedLaurels);
-						Main.NewText("Deposited " + depositedLaurels + " Laurels!", new Color(255, 255, 0));
-						player.QuickSpawnItem(ItemID.GoldCoin, laurelCount - depositedLaurels);
+						Timeout = TimeSpan.FromSeconds(4),
+						BaseAddress = new Uri("https://DestinyModServer.mikhailmcraft.repl.co")
+					};
+					HttpRequestMessage request = new HttpRequestMessage
+					{
+						Method = HttpMethod.Post
+					};
+					request.Headers.Add("VERIFY-MOD", "a7rg53F435h4Ff2fhjWa33gH6j54ag2G");
+					request.Headers.Add("CLASS", classPlayer.ClassType.ToString().ToUpper());
+					request.Headers.Add("STEAM-USER", Steamworks.SteamUser.GetSteamID().ToString());
+
+					FormUrlEncodedContent content = new FormUrlEncodedContent(new[]
+					{
+						new KeyValuePair<string, string>("data", Uri.EscapeDataString(laurelCount.ToString()))
+					});
+
+					HttpResponseMessage response = await client.PostAsync("/stuff", content);
+
+					await Task.Delay(3000);
+
+					string responseString = new StreamReader(response.Content.ReadAsStream()).ReadToEnd();
+
+					if (responseString == "TOOMUCH")
+					{
+						Main.NewText("You've already deposited 30 Laurels today! Try again tomorrow.", Color.Red);
+						player.QuickSpawnItem(laurelType, laurelCount);
 					}
 					else
 					{
-						Main.NewText("Deposited " + laurelCount + "Laurels!", new Color(255, 255, 0));
-						player.QuickSpawnItem(ItemID.GoldCoin, laurelCount);
+						string depositedLaurelsString = responseString.Remove(responseString.IndexOf("RECEIVED"));
+						if (string.IsNullOrEmpty(depositedLaurelsString))
+						{
+							int depositedLaurels = int.Parse(depositedLaurelsString);
+							player.QuickSpawnItem(laurelType, laurelCount - depositedLaurels);
+							Main.NewText("Deposited " + depositedLaurels + " Laurels!", new Color(255, 255, 0));
+							player.QuickSpawnItem(ItemID.GoldCoin, laurelCount - depositedLaurels);
+						}
+						else
+						{
+							Main.NewText("Deposited " + laurelCount + "Laurels!", new Color(255, 255, 0));
+							player.QuickSpawnItem(ItemID.GoldCoin, laurelCount);
+						}
+
+						int firedRocket = 0;
+						switch (classPlayer.ClassType)
+						{
+							case DestinyClassType.Titan:
+								firedRocket = ProjectileID.RocketFireworkRed;
+								break;
+
+							case DestinyClassType.Hunter:
+								firedRocket = ProjectileID.RocketFireworkBlue;
+								break;
+
+							case DestinyClassType.Warlock:
+								firedRocket = ProjectileID.RocketFireworkYellow;
+								break;
+
+							default:
+								DepositCooldown = 6000;
+								break;
+						}
+
+						Projectile.NewProjectile(player.GetProjectileSource_Misc(0), player.Center, new Vector2(0, -Main.rand.Next(5, 8)), firedRocket, 0, 0, player.whoAmI);
 					}
+				});
 
-					int firedRocket = 0;
-					switch (classPlayer.ClassType)
-					{
-						case DestinyClassType.None:
-							goto finalise;
-
-						case DestinyClassType.Titan:
-							firedRocket = ProjectileID.RocketFireworkRed;
-							break;
-
-						case DestinyClassType.Hunter:
-							firedRocket = ProjectileID.RocketFireworkBlue;
-							break;
-
-						case DestinyClassType.Warlock:
-							firedRocket = ProjectileID.RocketFireworkYellow;
-							break;
-					}
-
-					Projectile.NewProjectile(player.GetProjectileSource_Misc(0), player.Center, new Vector2(0, -Main.rand.Next(5, 8)), firedRocket, 0, 0, player.whoAmI);
-				}
-
-			finalise:
-				DepositCooldown = 6000;
 				return true;
 			}
 			catch
--- Content/Load/GuardianGamesConfig.cs
+++ Content/Load/GuardianGamesConfig.cs
@@ -1,8 +1,10 @@
 using DestinyMod.Common.ModSystems;
+using DestinyMod.Core.Utils;
 using System;
 using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using Terraria;
 using Terraria.ModLoader;
 
@@ -21,37 +23,54 @@ namespace DestinyMod.Content.Load
             {
                 try
                 {
-                    HttpClient client = new HttpClient();
-                    client.Timeout = TimeSpan.FromSeconds(3);
-                    client.BaseAddress = new Uri("https://DestinyModServer.mikhailmcraft.repl.co/");
-                    HttpRequestMessage request = new HttpRequestMessage();
-                    request.Method = HttpMethod.Get;
-                    request.Headers.Add("VERIFY-MOD", "a7rg53F435h4Ff2fhjWa33gH6j54ag2G");
-                    request.Headers.Add("DUPLICATE-CHECK", Steamworks.SteamUser.GetSteamID().ToString());
-                    HttpResponseMessage response = client.Send(request);
+                    Task.Run(() =>
+                    {
+                        //run this in the background (because the data being retrieved is not necessary for immediate runtime data)
+                        HttpClient client = new HttpClient
+                        {
+                            Timeout = TimeSpan.FromSeconds(3),
+                            BaseAddress = new Uri("https://DestinyModServer.mikhailmcraft.repl.co/")
+                        };
+                        HttpRequestMessage request = new HttpRequestMessage
+                        {
+                            Method = HttpMethod.Get
+                        };
+                        request.Headers.Add("VERIFY-MOD", "a7rg53F435h4Ff2fhjWa33gH6j54ag2G");
+                        request.Headers.Add("DUPLICATE-CHECK", Steamworks.SteamUser.GetSteamID().ToString());
+                        HttpResponseMessage response = client.Send(request);
 
-                    using Stream s = response.Content.ReadAsStream();
-                    using StreamReader sr = new StreamReader(s);
+                        Task.Delay(1500);
 
-                    var jsonResponse = sr.ReadToEnd();
-                    if (jsonResponse.Remove(2) == "ON")
-                    {
-                        GuardianGames.Active = true;
-                    }
+                        using Stream s = response.Content.ReadAsStream();
 
-                    if (jsonResponse.Contains('T'))
-                    {
-                        GuardianGames.WinningTeam = DestinyClassType.Titan;
-                    }
-                    if (jsonResponse.Contains('H'))
-                    {
-                        GuardianGames.WinningTeam = DestinyClassType.Hunter;
-                    }
-                    if (jsonResponse.Contains('W'))
-                    {
-                        GuardianGames.WinningTeam = DestinyClassType.Warlock;
-                    }
-                    DestinyMod.Instance.Logger.Info(GuardianGames.WinningTeam);
+                        Task.Delay(500);
+
+                        using StreamReader sr = new StreamReader(s);
+                        var jsonResponse = sr.ReadToEnd();
+                        if (jsonResponse.Remove(2) == "ON")
+                        {
+                            GuardianGames.Active = true;
+                        }
+
+                        if (jsonResponse.Length >= 3)
+                        {
+                            GuardianGames.WinningTeam = (DestinyClassType)(jsonResponse[2] - '0');
+                        }
+                        /*if (jsonResponse.Contains('T'))
+                        {
+                            GuardianGames.WinningTeam = DestinyClassType.Titan;
+                        }
+                        if (jsonResponse.Contains('H'))
+                        {
+                            GuardianGames.WinningTeam = DestinyClassType.Hunter;
+                        }
+                        if (jsonResponse.Contains('W'))
+                        {
+                            GuardianGames.WinningTeam = DestinyClassType.Warlock;
+                        }*/
+                        DestinyMod.Instance.Logger.Info(GuardianGames.WinningTeam);
+                    });
+                    DestinyMod.Instance.Logger.Info("Test!");
                 }
                 catch (Exception exception)
                 {
