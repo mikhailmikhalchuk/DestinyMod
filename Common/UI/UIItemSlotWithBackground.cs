6d6dd3a686d0b1534e53504ca9c929c2b7dcbd09
Smug B <43657791+SmugBlanco@users.noreply.github.com>
2022-05-02 11:30:06 -0400

    Functional Item Slot

--- Common/UI/UIItemSlotWithBackground.cs
+++ Common/UI/UIItemSlotWithBackground.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.GameContent;
 using Terraria.GameInput;
 using Terraria.UI;
 
 namespace DestinyMod.Common.UI
 {
+    // This only handles items in stacks of 1
     public class UIItemSlotWithBackground : UIImageWithBackground
     {
         private Item InternalItem;
@@ -36,6 +39,8 @@ namespace DestinyMod.Common.UI
 
         public event Action<UIItemSlotWithBackground> OnUpdateItem;
 
+        public Player Player => Main.LocalPlayer;
+
         public UIItemSlotWithBackground(Texture2D background, int? scaleSize = null, Item item = null, Func<Item, bool> isItemValid = null) : base(background, scaleSize: scaleSize ?? background.Width)
         {
             InternalItem = new Item();
@@ -52,18 +57,123 @@ namespace DestinyMod.Common.UI
                 return;
             }
 
-            if (!PlayerInput.IgnoreMouseInterface && ContainsPoint(Main.MouseScreen))
+            if (!PlayerInput.IgnoreMouseInterface)
             {
                 Main.LocalPlayer.mouseInterface = true;
-
-                if (IsItemValid == null || IsItemValid(Main.mouseItem))
-                {
-                    Item = Main.mouseItem;
-                    OnUpdateItem?.Invoke(this);
-                }
+                HandleLeftClick();
             }
         }
 
-        public void InvokeOnUpdateItem() => OnUpdateItem?.Invoke(this); // Supposedly bad practice but I do not care
+        public override void RightClick(UIMouseEvent evt)
+        {
+            base.RightClick(evt);
+
+            if (BlockItemInput)
+            {
+                return;
+            }
+
+            if (!PlayerInput.IgnoreMouseInterface)
+            {
+                Main.LocalPlayer.mouseInterface = true;
+                HandleRightClick();
+            }
+        }
+
+        public void SwapMouseAndCurrentItem()
+        {
+            Item currentItem = Item.Clone();
+            Item = Main.mouseItem;
+            Main.mouseItem = currentItem;
+        }
+
+        public void ConsumeMouseItem()
+        {
+            Main.mouseItem.stack--;
+            if (Main.mouseItem.stack < 0)
+            {
+                Main.mouseItem.SetDefaults();
+            }
+        }
+
+        public void HandleLeftClick()
+        {
+            IEntitySource uiItemSlotEntitySource = Player.GetSource_Misc("UI");
+            if (Main.keyState.IsKeyDown(Keys.LeftShift) || Main.keyState.IsKeyDown(Keys.RightShift))
+            {
+                Player.QuickSpawnClonedItem(uiItemSlotEntitySource, Item);
+                Item = new Item();
+                goto ret;
+            }
+
+            Item currentItem = Item.Clone();
+            if (Main.mouseItem.IsAir)
+            {
+                Main.mouseItem = currentItem;
+                Item = new Item();
+                goto ret;
+            }
+
+            if (IsItemValid == null || IsItemValid(Main.mouseItem))
+            {
+                Item = Main.mouseItem;
+                ConsumeMouseItem();
+                if (Main.mouseItem.IsAir)
+                {
+                    Main.mouseItem = currentItem;
+                    goto ret;
+                }
+
+                if (Main.mouseItem.type == currentItem.type && Main.mouseItem.stack < Main.mouseItem.maxStack)
+                {
+                    Main.mouseItem.stack++;
+                }
+                else
+                {
+                    Player.QuickSpawnClonedItem(uiItemSlotEntitySource, currentItem);
+                }
+            }
+
+            ret: // Evil beyond belief
+            OnUpdateItem?.Invoke(this);
+        }
+
+        public void HandleRightClick()
+        {
+            IEntitySource uiItemSlotEntitySource = Player.GetSource_Misc("UI");
+
+            Item currentItem = Item.Clone();
+            if (Main.mouseItem.IsAir)
+            {
+                Main.mouseItem = currentItem;
+                Item = new Item();
+                goto ret;
+            }
+
+            if (IsItemValid == null || IsItemValid(Main.mouseItem))
+            {
+                Item = Main.mouseItem;
+                ConsumeMouseItem();
+                if (Main.mouseItem.IsAir)
+                {
+                    Main.mouseItem = currentItem;
+                    goto ret;
+                }
+
+                if (Main.mouseItem.type == currentItem.type && Main.mouseItem.stack < Main.mouseItem.maxStack)
+                {
+                    Main.mouseItem.stack++;
+                }
+                else
+                {
+                    Player.QuickSpawnClonedItem(uiItemSlotEntitySource, currentItem);
+                }
+            }
+
+            ret: // Evil beyond belief
+            OnUpdateItem?.Invoke(this);
+        }
+
+        public void InvokeOnUpdateItem() => OnUpdateItem?.Invoke(this); // Supposedly bad practice but Is dos nots cares
     }
 }
