1aefb2fade5322c375f79e0cac625e013a6ea2a6
Cuno <59270079+MikhailMCraft@users.noreply.github.com>
2022-05-13 18:28:35 -0400

    Unscuff the system

--- Content/Load/ItemSlotPerkDetection.cs
+++ Content/Load/ItemSlotPerkDetection.cs
@@ -1,4 +1,5 @@
-﻿using DestinyMod.Content.UI.ItemDetails;
+﻿using DestinyMod.Common.GlobalItems;
+using DestinyMod.Content.UI.ItemDetails;
 using Terraria;
 using Terraria.Audio;
 using Terraria.ID;
@@ -8,32 +9,28 @@ namespace DestinyMod.Content.Load
 {
     public sealed class ItemSlotPerkSystem : ILoadable
     {
-        private double _past;
-
         public void Load(Mod mod)
         {
-            On.Terraria.UI.ItemSlot.MouseHover_ItemArray_int_int += ItemSlot_MouseHover_ItemArray_int_int;
+            On.Terraria.UI.ItemSlot.RightClick_ItemArray_int_int += ItemSlot_RightClick_ItemArray_int_int;
         }
 
-        private void ItemSlot_MouseHover_ItemArray_int_int(On.Terraria.UI.ItemSlot.orig_MouseHover_ItemArray_int_int orig, Item[] inv, int context, int slot)
+        private void ItemSlot_RightClick_ItemArray_int_int(On.Terraria.UI.ItemSlot.orig_RightClick_ItemArray_int_int orig, Item[] inv, int context, int slot)
         {
             orig.Invoke(inv, context, slot);
 
-            if (Main.gameTimeCache.TotalGameTime.TotalMilliseconds - _past < 300)
+            if (inv[slot].IsAir || !Main.mouseRight || !Main.mouseRightRelease)
             {
                 return;
             }
-            if (Main.mouseRight && ModContent.GetInstance<ItemDetailsState>().UserInterface.CurrentState == null)
+            if (ModContent.GetInstance<ItemDetailsState>().UserInterface.CurrentState == null)
             {
                 SoundEngine.PlaySound(SoundID.MenuOpen);
                 ModContent.GetInstance<ItemDetailsState>().UserInterface.SetState(new ItemDetailsState(inv[slot]));
-                _past = Main.gameTimeCache.TotalGameTime.TotalMilliseconds;
             }
-            else if (Main.mouseRight && ModContent.GetInstance<ItemDetailsState>().UserInterface.CurrentState != null)
+            else
             {
                 SoundEngine.PlaySound(SoundID.MenuClose);
                 ModContent.GetInstance<ItemDetailsState>().UserInterface.SetState(null);
-                _past = Main.gameTimeCache.TotalGameTime.TotalMilliseconds;
             }
         }
 
